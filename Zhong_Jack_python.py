c74c62f23b328792206db689f9e8dd3d9f79e361
jqzhong2572 <jz513@georgetown.edu>
2016-09-14 02:32:49 -0400

    9/14 2am draft HW1
    whee i've done it

--- Zhong_Jack_python.py
+++ Zhong_Jack_python.py
@@ -15,15 +15,16 @@ def eng_sw(word):
         return word # if the word isn't in dictionary, return the original
 
 def translate(text): # the funtion itself
-    words = text.split( ) # get each word from the string
+    words = text.split( ) # get each word from the string using split()function
     output = "" # initialize
     for i in words:
-        output += eng_sw(i) + " " # add each word to the output
+        output = output + eng_sw(i) + " " # add each word to the output
     return output
 
-'''try
+
 translate('merry christmas Lalala 123')
-'''
+
+
 
 
 # --------------------Question 2--------------------
@@ -41,10 +42,8 @@ def char_freq(s):
             dic[i] = 1 # if key is not in dic, create it
     return dic    
     
-'''try
-char_freq('abcabczz')
-'''
 
+char_freq('abbabcbdbabdbdbabababcbcbab')
 
 
 
@@ -54,36 +53,114 @@ char_freq('abcabczz')
 encoder/decoder of ROT-13.
 '''
 
-key = {'a':'n', 'b':'o',
-'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v',
-'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c',
-'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j',
-'x':'k', 'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q',
-'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X',
-'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E',
-'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L',
-'Z':'M'}
+def decoder(string):
+    key = {'a':'n', 'b':'o',
+           'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v',
+           'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c',
+           'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j',
+           'x':'k', 'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q',
+           'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X',
+           'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E',
+           'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L',
+           'Z':'M'} # dictionary
+    output = '' # where the result is stored
+    for i in string:
+        if i in key:
+            output += key[i] # if the character is in key, use decode
+        else:
+            output += i # if the character is not in key, return original
+    return output
+
 
 string = 'Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!'
-output = '' 
+decoder(string)
 
-for i in string:
-    if i in key:
-        output += key[i] # if the character is in key, use decode
-    else:
-        output += i # if the character is not in key, return original
-    
-print (output)
 
 
 
 # --------------------Question 4--------------------
+'''
+function correct() takes a string and sees to it that 
+1) two or more occurrences of the space character is compressed into one, and 
+2) inserts an extra space after a period 
+   if the period is directly followed by a letter. 
+'''
+
+import re
+def correct(string):
+    string = re.sub('\ +', ' ', string) # if it matches spaces 
+                                        # (\ is for escaping control character)
+    string = re.sub('\.', '. ', string) # if it matches . with no space after
+    return string
+   
+   
+correct("This is very       funny and cool.Indeed!")
+ 
+
 
 
 # --------------------Question 5--------------------
+'''
+function make_3sg_form() is given a verb in infinitive
+form and returns its third person singular form. 
+a. If the verb ends in y, remove it and add ies 
+b. If the verb ends in o, ch, s, sh, x or z, add es 
+c. By default just add s 
+'''
+
+import re
+def make_3sg_form(verb):
+    if verb.endswith('y'): # if the verb ends in y
+        return re.sub('y', 'ies', verb) # substitute y for ies
+    elif verb.endswith(('o','ch','s','sh','x','z')): # if ends in those
+        return verb + 'es' # add es
+    else:
+        return verb + 's' # all other cases add s   
+
+
+make_3sg_form('try')
+make_3sg_form('brush')
+make_3sg_form('run')
+make_3sg_form('fix')
+
+
 
 
 # --------------------Question 6--------------------
+'''
+function make_ing_form() is given a verb in infinitive form and returns its
+present participle form
+a. If the verb ends in e, drop the e and add ing (if not exception: be,
+   see, flee, knee, etc.) 
+b. If the verb ends in ie, change ie to y and add ing 
+c. For words consisting of consonant-vowel-consonant, double the final letter 
+   before adding ing 
+d. By default just add ing 
+'''
+
+import re  
+exception = ['be','see','flee','knee'] # you can add more exceptions here 
+vowel = ['a','e','i','o','u'] # vowel list
+
+def make_ing_form(verb):
+    if verb.endswith('ie'): # do ie before e because it is a special case
+        return re.sub('ie', 'ying', verb) # ie to ying
+    elif verb.endswith('e') and verb not in exception: # ends with e but not 
+                                                       # in exception
+        return re.sub('e', 'ing', verb) # e to ing
+    elif verb[-3] not in vowel and verb[-2] in vowel and verb[-1] not in vowel:
+                            # the last 3 digits represented by[-3],[-2],[-1]
+                            # letter not in vowel is in consonant
+        return verb + verb[-1] + 'ing' # double final letter and add ing
+    else:
+        return verb + 'ing' # in all other cases, add ing
+
+
+make_ing_form('lie')
+make_ing_form('see')
+make_ing_form('move')
+make_ing_form('hug')
+
 
 
 
@@ -94,16 +171,21 @@ function max_in_list() takes a list of numbers and
 returns the largest one
 '''
 
-def max_in_list(*num): #return the biggest number in tuple
+from functools import reduce
+def max1(*num): # return the biggest number in tuple
     biggest = 0 
     for i in num:
         if i > biggest:
-            biggest = i
+            biggest = i # replace biggest by the biggest item in list
     return biggest
   
-from functools import reduce  
-max_num = reduce (max_in_list,[1,3,76,4,1,34,99]) # user may modify this line
-print (max_num)
+  
+def max_in_list(list1):
+    return reduce (max1,list1) # iterate max funtion for list
+    
+
+list1 = [1,3,56,2,64]
+max_in_list(list1)
 
 
 
@@ -126,26 +208,124 @@ def map_to_lengths_map(words):
 
 
 def map_to_lengths_lists(words):
-    return words, [len(word) for word in words]
+    return words, [len(word) for word in words] # count them 1 by 1 and print
     
 
-'''try these
 map_to_lengths_for (['words','adf','adfa','12'])
 map_to_lengths_map (['words','adf','adfa','12'])
 map_to_lengths_lists (['words','adf','adfa','12'])
-'''
+
 
 
 
 # --------------------Question 9--------------------
 
+
+'''
+function find_longest_word() takes a list of words
+and returns the length of the longest one. 
+'''
+
+from functools import reduce
+def find_longest_word(words):
+    return reduce (max, list(map(len,words))) 
+        # first use Question 8's map_to_lengths_map to get a list of length
+        # secondly repeatedly take max of the first two numbers and replace
+    
+    
+words = ['happy', 'satisfy', 'four']
+find_longest_word(words)
+
+
+
+
 # --------------------Question 10--------------------
+'''
+function filter_long_words() takes a list of words and an integer
+n and returns the list of words that are longer than n.
+'''
+
+def filter_long_words(words, n):
+    return list(filter(lambda word: len(word) > n, words))
+        # first create a temp. function that compares length of a word to n
+        # then filter out words whose value is True
+    
+    
+words = ['hello','my','name','is','Jacklalala']
+filter_long_words(words, 3)
+
+
+
 
 # --------------------Question 11--------------------
+'''
+function translate() takes a list of English words and
+returns a list of Swedish words
+'''
+
+def translate(words):
+    dic = {"merry":"god","christmas":"jul","and":"och","happy":"gott",
+           "new":"nytt","year":"år"} # dictionary
+    return list(map(lambda word: dic.get(word), words))
+        # first create a temp. function to get the value from dictionary
+        # then use map function to iterate and return as a list
+
+words = ["merry","christmas","and","happy","new","year","123"]
+translate(words)
+
+
+
 
 # --------------------Question 12--------------------
+'''
+Implement the higher order functions map(), filter()and
+reduce()
+'''
+
+def map1(function, list1):
+    output = [] # initialize output
+    for i in list1:
+        output.append(function(i)) # add function results 1 by 1 to output
+    return output
+    
+'''test example'''   
+list1 = [1,3,5,7,1,3,45,999,123]
+def function(x):
+    x += 1
+map1(function, list1)    
+#-------------------------------------------------------------------    
 
 
+def filter1(function, list1):
+    output = [] # initialize output
+    for i in list1:
+        if function(i) == True: # if item i passes filter
+            output.append(i) # add i to output list
+    return output
+        
+'''test example'''  
+list1 = [1,3,5,7,1,3,45,999,123]
+def function(x):
+    if x > 15:
+        return True
+    else:
+        return False
+filter1(function, list1)        
+
+#-------------------------------------------------------------------
+def reduce1(function, list1):
+    output = list1 # casually copy the list XD
+    while len(output) > 1: # when the list has more than 1 element
+        output[1] = function(list1[0],list1[1])
+                # replace the second element with result of function using the
+                # first and second elements so the list is reduced once
+        del list1[0] # delete the first item so the second becomes the first
+                     # in the next iteration
+    return output
+    
+'''test example'''
+list1 = [1,3,5,7,1,3,45,999,123]
+reduce1(max, list1)
 
 
 
